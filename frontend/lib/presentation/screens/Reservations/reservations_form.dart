269b402397c3f6f9dbb7bdb0b455caac07ee6456
kecir-mohamed <mohamed.kecir@ensia.edu.dz>
2025-12-04 21:49:23 +0100

    changed the paths

--- frontend/lib/presentation/screens/Reservations/reservations_form.dart
+++ frontend/lib/presentation/screens/Reservations/reservations_form.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../theme/app_colors.dart';
+import 'package:frontend/presentation/theme/app_colors.dart';
 
 class ReservationFormScreen extends StatefulWidget {
   final String pharmacyId;
